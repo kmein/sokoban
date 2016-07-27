6f312681a0184037dfb644e23916c2745e17395c
kmein <kieran.meinhardt@gmail.com>
2016-07-27 10:15:22 +0200

    Add command line option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
-import qualified Game.Sokoban.Console as Console (main)
-import qualified Game.Sokoban.Gtk as Gtk (main)
+import qualified Game.Sokoban.Console as Console (mainWith)
+import qualified Game.Sokoban.Gtk as Gtk (mainWith)
+
+import Options.Applicative
 
 main :: IO ()
-main = Gtk.main
+main =
+    do level <- execParser $
+           info (helper <*> sokobanOptions)
+               (fullDesc <> progDesc "Play a GTK clone of Sokoban")
+       Gtk.mainWith level
+    where sokobanOptions = argument auto (metavar "N" <> help "Select level N")
